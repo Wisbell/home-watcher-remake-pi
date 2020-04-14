7f8499d8d7e609d88e7da33ab27871f78ba3dcc1
wikiWookie <coolwest@gmail.com>
2020-04-14 14:04:47 -0500

    Add comment for filename function.  Refactor filename function

--- modules/takePicture.js
+++ modules/takePicture.js
@@ -1,22 +1,30 @@
 const { exec } = require("child_process");
 const path = require('path');
 
-// Create date for picture file name
+/**
+ * Create file name using current date.
+ * Removes parenthesis and colon in order to be worked with on a windows file system
+ * @returns {string} Current date as file name
+ */
 let createFileNameAsDate = () => {
   let date = new Date();
-  return date.toString().replace("(", ":").replace(")", ":").split(" ").join("_") + ".jpg";
+  return date.toString()
+    .replace("(", "")
+    .replace(")", "")
+    .replace(/:/g, '-')
+    .split(" ")
+    .join("_") + ".jpg";
 }
 
 /**
  * Takes picture with raspbian raspistill command line utility.
  * @returns {string} Path to new picture
  */
-
 module.exports.takePicture = () => {
   return new Promise( (resolve, reject) => {
 
     // Create argument to pass to execute raspistill
-    let fileName = createFileNameAsDate().replace(/:/g, '');
+    let fileName = createFileNameAsDate();
     console.log('fileName', fileName);
     let createPath = path.join(process.cwd(), fileName);
     let cameraArgument = [ "/opt/vc/bin/raspistill", "-vf -hf", "-n", "-q 10", "-t 1", "-o", createPath ].join(" ");
